6b9ee66807e45d064b580c0515097ed6a38ed319
Michael S <user@mail.com>
2018-10-10 23:21:37 -0700

    Finalized

--- README.md
+++ README.md
@@ -1,6 +1,29 @@
-# liri-node-app
-# LIRI Bot
+liri-node-app
+Requirements
 
-### Overview
+Make a Node.js app that depends on user input from the command line
+Integrate Spotify, and OMDb APIs via the appropriate NPM modules
+Use API calls and parse through returned JSON objects, outputting them in a specified format
+Read commands and queries from file
 
-In this assignment, you will make LIRI. LIRI is like iPhone's SIRI. However, while SIRI is a Speech Interpretation and Recognition Interface, LIRI is a _Language_ Interpretation and Recognition Interface. LIRI will be a command line node app that takes in parameters and gives you back data.
+Technologies Used
+
+Node.js
+JavaScript
+Spotify API (via spotify npm module)
+OMDb API (via request npm module)
+Bands In Town API (via request npm module)
+
+Code Explanation
+
+These app does the following functions after inputing your name:
+1. Concert search.
+2. Song search.
+3. Movie search
+4. Selecting "Search for something" returns a response of "i dont get it."
+5. Selecting "Nothing" returns a reponse of "Thank you for using Liri! Michael"
+
+The program also makes a request to the Spotify API, and we get back a JSON object that includes everything we need (artist(s), song, preview link, and album)
+The program also makes a HTTP request to the OMDb API using the request NPM module, and we get back a JSON object that includes everything we need (title, year, IMDb rating, language, etc.)
+
+https://youtu.be/fL8bO6jBrHo
--- liri.js
+++ liri.js
@@ -1,34 +1,25 @@
-require('dotenv').config();
-
+require("dotenv").config();
 var inquirer = require("inquirer");
-
 var fs = require("fs");
-
-var moment = require("moment");
-
+var moment = require('moment');
 var request = require("request");
-
 var keys = require("./keys.js");
-
 var Spotify = require("node-spotify-api");
-
-var spotify = new spotify(keys.spotify);
-
+var spotify = new Spotify(keys.spotify);
 var username;
-
 var action;
-
 var item;
-
 inquirer
     .prompt({
         type: "input",
         message: "What is your name?",
         name: "username"
-    })
-    .then(function (response) {
+    },)
+    .then(function(response){
+        username = response.username;
         actionPrompt();
     })
+
 
 function actionPrompt() {
     inquirer
@@ -36,13 +27,13 @@ function actionPrompt() {
             {
                 message: "What would you like to do?",
                 type: "list",
-                choice: ["Search for a Concert", "Search for a Song", "Search for a movie", "Search for something", "Nothing"],
+                choices: ["Search for a Concert", "Search for a Song", "Search for a movie", "Search for something","Nothing"],
                 name: "action"
             },
         ])
         .then(function (inquirerResponse) {
             action = inquirerResponse.action;
-
+            
             switch (action) {
                 case "Search for a Concert":
                     promptForQuery("concert");
@@ -57,130 +48,133 @@ function actionPrompt() {
                     heyLiri("do-what-it-says");
                     break;
                 case "Nothing":
-                    console.log("Thank You for using LIRI! " + username);
+                    console.log("Thank you for using Liri! " + username);
                     break;
             }
+            
+
         });
-    function concert() {
-        var artist = item;
-        request("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp", function (error, response, body) {
+}
+function concert(){
+    var artist = item;
+    request("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp", function(error, response, body) {
+       
+        // console.log(JSON.parse(body, null, 2));
+        if (!error && response.statusCode === 200) {
+      
+            JSON.parse(body).forEach( function(event){
 
-            //console.log(JSON.parse(body, null, 2));
-            if (!error && response.statusCode === 200) {
-
-                JSON.parse(body).forEach(function (event) {
-                    console.log("*********************");
-                    console.log("Venue: " + event.venue.name);
-                    console.log("City: " + event.venue.city);
-
-                    //using moment to format date
-                    var date = moment(event.datetime);
-                    console.log("When: " + date.format("MM/DD/YYYY"));
-                });
-                actionPrompt();
-            };
-        });
-
-    };
-    function spotifySong(item) {
-        var song = item;
-
-        spotify.search({ type: 'track', query: song }, function (err, data) {
-            if (err) {
-                return console.log("Error occurred: " + err);
-            }
-
-            //console.log(data);
-            var playList = data.tracks.items;
-
-            playList.forEach(function (elements) {
-                console.log("*********************");
-                console.log(elements.artist[0].name)
-                console.log(elements.name)
-                console.log(elements.href)
-                console.log(elements.album.name)
-            })
-            console.log("*******************");
-            console.log("I found " + playList.length + " songs.");
+                console.log("******************");
+                console.log("Venue: " + event.venue.name);
+                console.log("City: " + event.venue.city);
+                // use momemnt to format date
+                var date = moment(event.datetime);
+                console.log("When: " + date.format("MM/DD/YYYY"));
+            });
             actionPrompt();
-        });
-    };
-    function movie(item) {
-        var movie = item;
-        request("http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy", function (error, response, body) {
-
-            //If request was successful
-            if (!error && response.statusCode === 200) {
-
-                //Then log the following from the site!
-                console.log("****************************************")
-                console.log("The Title: " + JSON.parse(body).Title);
-                console.log("Year:" + JSON.parse(body).Year);
-                console.log("imdb Rating: " + JSON.parse(body).imdbRating);
-                console.log("Rotton Tomatos: " + JSON.parse(body).Ratings[1].Value);
-                console.log("Produced in: " + JSON.parse(body).Country);
-                console.log("Languages: " + JSON.parse(body).Language);
-                console.log("Plot: " + JSON.parse(body).Plot);
-                console.log("Actors: " + JSON.parse(body).Actors);
-                console.log("***************************************")
-            }
-
-            actionPrompt();
-        });
-    };
-    function doWhat() {
-        fs.readFile("random.txt", "UTF8", function (error, data) {
-            if (error) {
-                return console.log(error);
-            }
-            var data = data.split(",");
-            item = data[1];
-            heyLiri(data[0]);
-        })
-    };
-    function heyLiri(action) {
-        switch (action) {
-            case "concert":
-                concert(item);
-                break;
-            case "song":
-                spotifySong(item);
-                break;
-            case "movie":
-                if (!item) {
-                    console.log("*******You did not pick a movie.********")
-                    item = "Mr. Nobody"
-                }
-                movie(item);
-                break;
-            case "do-what-it-says":
-                doWhat(item);
-                break;
-            default:
-                console.log("I Don't Get It.")
         };
+    });  
+};
+function spotifySong(item){
+    var song = item;
+    
+    spotify.search({ type: 'track', query: song }, function(err, data) {
+    if (err) {
+        return console.log('Error occurred: ' + err);
+    }
+    //  console.log(data)
+    var playList = data.tracks.items;
 
-    }
-    function logIt() {
-        fs.appendFile("log.txt", action + ", " + item + ", ", function (error) {
-            if (error) {
-                console.log("It could not be log becaue " + error)
-            }
-        })
-    }
-    function promptForQuery(query) {
-        inquirer
-            .prompt(
-                {
-                    type: "input",
-                    message: "What " + query + " should I look?",
-                    name: "item"
-                }
-            )
-            .then((response) => {
-                item = response.item;
-                logIt();
-                heyLiri(query)
-            })
-    }
+    playList.forEach(function(elements) {
+        console.log('*******************')
+        console.log(elements.artists[0].name) 
+        console.log(elements.name)
+        console.log(elements.href)
+        console.log(elements.album.name)
+    })
+    console.log('*******************')
+    console.log("I found "+playList.length+ " songs.")
+    actionPrompt();
+    });
+};
+function movie(item){
+    var movie = item;
+    request("http://www.omdbapi.com/?t="+ movie + "&y=&plot=short&apikey=trilogy", function(error, response, body) {
+
+        // If the request was successful...
+        if (!error && response.statusCode === 200) {
+      
+          // Then log the body from the site!
+          console.log("****************************************")
+          console.log("The Title: "+JSON.parse(body).Title);
+          console.log("Year:" + JSON.parse(body).Year);
+          console.log("imdb Rating: " + JSON.parse(body).imdbRating);
+          console.log("Rotton Tomatos: " + JSON.parse(body).Ratings[1].Value);
+          console.log("Produced in: " + JSON.parse(body).Country);
+          console.log("Languages: " + JSON.parse(body).Language);
+          console.log("Plot: " + JSON.parse(body).Plot);
+          console.log("Actors: " + JSON.parse(body).Actors);
+          console.log("***************************************")
+         
+          
+        }
+        
+        actionPrompt();
+      });
+};
+function dowhat(){
+    fs.readFile("random.txt","utf8",function (error, data){
+        if (error) {
+            return console.log(error);
+          }
+        var data = data.split(",");
+        item = data[1];
+        heyLiri(data[0]);
+         
+    })
+};
+function heyLiri(action){
+    switch(action){
+        case "concert":
+        concert(item);
+        break;
+        case "song":
+        spotifySong(item);
+        break;
+        case "movie":
+        if (!item){
+            console.log("*******You did not pick a movie.********")
+            item = "Mr. Nobody"
+        }
+        movie(item);
+        break;
+        case "do-what-it-says":
+        dowhat(item);
+        break;
+        default:
+        console.log("i dont get it.")  
+    };
+     
+}
+function logIt(){
+    fs.appendFile("log.txt", action +", " + item + ", ",function(error){
+        if(error){
+            console.log("It could not be log becaue " + error)
+        }
+    })
+}
+function promptForQuery(query){
+    inquirer
+    .prompt(
+        {
+        type: "input",
+        message: "What "+query+" should I look?",
+        name: "item"
+        }
+    )
+    .then((response)=>{
+        item = response.item;
+        logIt();
+        heyLiri(query)
+    })
 }
